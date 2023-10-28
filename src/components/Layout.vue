f2f30b3a590ee5a1f72828fd48d8cb7e027697b9
lsimon <leo@lsimon.org>
2023-10-28 07:15:12 -0400

    tailwind config with colors

---
+++ src/components/Layout.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col w-full bg-light-grey min-h-screen">
+        <div class="bg-purple h-24 flex flex-col justify-center">
+            <span class="text-white text-2xl px-4">Trivia</span>
+        </div>
+        <slot />
+    </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.vue",
-  ],
+  content: ["./index.html", "./src/**/*.vue"],
   theme: {
     extend: {},
+    colors: {
+      "purple": "#D41EB5",
+      "purple-darker": "#A80A97",
+      "purple-lighter": "#FA88D9",
+      "purple-light": "#FEBEE8",
+      "purple-extra-light": "#FFF2FA",
+      "blue": "#0A4BCC",
+      "blue-lighter": "#7C97F9",
+      "blue-light": "#B8C3FE",
+      "light-grey": "#EBE6E9",
+      "almost-white": "#FCFAFB",
+      "white": "#FFFFFF",
+      "black": "#262226",
+      "dark-grey": "#4B444B",
+      "green": "#44BD32",
+      "orange": "#D35400",
+      "red": "#E84118",
+      "yellow": "#FBC531"
+    },
   },
   plugins: [],
-}
-
+};
